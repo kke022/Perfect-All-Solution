acf803f6739f619ff56118f4743832026bb557bb
kke022 <jsela68@naver.com>
2016-10-14 01:31:11 +0900

    Ch03의 Prj11수행
    소스 numliteral.c

---
+++ Ch03/Prj11/numliteral.c
@@ -0,0 +1,26 @@
+/*
+솔루션 / 프로젝트 / 소스파일: Ch03 / prj11 / numliteral.c
+정수형 실수형 리터럴 상수의 다양한 표현
+v 1.0 2016.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d", 30);  print("%d", 10);
+	printf("%d", 030);  print("%d", 010);
+	printf("%d", 0X2F);  print("%d", 0x1b);
+
+
+	printf("%f", 3.14);  print("%f", 2.0);
+	printf("%f", 3.14E+2);  print("%f", 21.8e2);
+	printf("%f", 3.14E-2);  print("%f", 218e-3);
+
+
+
+
+
+
+	return 0;
+}
